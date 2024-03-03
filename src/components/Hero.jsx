d26887f867d7cbd5724f0d0528f59610e077b09d
farisasif7 <farisbinasif2016@gmail.com>
2024-03-03 14:16:18 +0500

    Added hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {logo} from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain'/>
+
+        <button 
+        type='button'
+        onClick={() => window.open('https://github.com/farisasif7/summarize-articles-ai-app')}
+        className='black_btn'>
+          Github
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md-hidden'/>
+        <span className='orange_gradient'> OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className='desc'>
+        Simplify your reading with SummArticle, 
+        an open-source article summarizer that transforms
+        long articles into short summaries.
+      </h2>
+
+    </header>
+  )
+}
+
+export default Hero
