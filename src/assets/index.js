227856f97e1ecaa0e429ed0b043ed1defa592fe6
farisasif7 <farisbinasif2016@gmail.com>
2024-03-04 12:23:47 +0500

    completed app

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,13 @@ import loader from './loader.svg'
 import copy from './copy.svg'
 import logo from './logo.svg'
 import tick from './tick.svg'
+import data_analysis from './data_analysis.png'
 
 export {
     linkIcon,
     loader,
     copy,
     logo,
-    tick
+    tick,
+    data_analysis
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {logo} from '../assets';
+import { data_analysis } from '../assets';
 
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain'/>
+        <img src={data_analysis} alt='summarticle_logo' className='w-28 object-contain'/>
 
         <button 
         type='button'
@@ -17,7 +18,7 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md-hidden'/>
-        <span className='orange_gradient'> OpenAI GPT-4</span>
+        <span className='blue_gradient'> OpenAI GPT-4</span>
       </h1>
 
       <h2 className='desc'>
